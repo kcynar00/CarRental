f08e1fba88440b4e0e075968462231753c6ae412
Konrad Cynar <kcynar00@gmail.com>
2025-12-29 18:02:36 +0100

    pierwsze kroki w froncie

---
+++ Pages/Cars.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarRental.Pages
+{
+    public class CarsModel : PageModel
+    {
+        public void OnGet()
+        {
+        
+        }
+    }
+}
